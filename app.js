37d27cbd4186ec88589ac32cfa5b35aa612da297
21Mewzyk <pallasigueryan98@gmail.com>
2024-06-15 13:08:56 +0800

    app.js fix routes

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
 const { errors } = require('celebrate');
 
+const registerRoute = require('./routes/registerRoute');
+const deleteUserRoute = require('./routes/deleteRoute');
+const getUserRoute = require('./routes/getUserRoute');
+const getAllUsersRoute = require('./routes/getAllUsersRoute');
+const updateUserRoute = require('./routes/updateUserRoute');
+
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 
-app.use('/users', routes);
+app.use('/users', registerRoute);
+app.use('/users', deleteUserRoute);
+app.use('/users', getUserRoute);
+app.use('/users', getAllUsersRoute);
+app.use('/users', updateUserRoute);
 
 app.use(errors());
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });

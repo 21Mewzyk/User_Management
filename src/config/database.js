f5f4770b896c7aad78f64a34fbe5ef534c0a603d
21Mewzyk2 <155867191+21Mewzyk@users.noreply.github.com>
2024-06-30 15:44:05 +0800

    ehhhh nani

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,15 @@ const config = require('./dbConfig');
 const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, {
     host: config.db.host,
     dialect: config.db.dialect,
-    dialectModule: require('mysql'),
     logging: console.log,
 });
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
